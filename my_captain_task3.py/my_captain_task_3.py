a878c20506e510c6b07cdea58b6834cc43a507e5
TANAYCHORDIA <109026465+TANAYCHORDIA@users.noreply.github.com>
2022-09-14 21:30:42 +0530

    Add files via upload

---
+++ my_captain_task3.py/my_captain_task_3.py
@@ -0,0 +1,14 @@
+list1=[12,-7,5,64,-14]
+for i in list1:
+    if i>0:
+        print(i,end=' ')
+
+print('')
+list2=[12,14,-95,3]
+x=[]
+for i in list2:
+    if i>0:
+        x.append(i)
+       
+print(x,end=' ')
+print('')
